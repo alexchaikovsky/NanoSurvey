e5e84cf649ea6c043400d5d1dd50e7e323194857
oldborrow37 <alex.chaikovsky@gmail.com>
2021-04-22 20:51:12 +0300

    Fix

--- NanoSurveyAPI/Models/NanoSurveyContext.cs
+++ NanoSurveyAPI/Models/NanoSurveyContext.cs
@@ -24,15 +24,6 @@ namespace NanoSurveyAPI.Models
         public virtual DbSet<Survey> Surveys { get; set; }
         public virtual DbSet<SurveyQuestion> SurveyQuestions { get; set; }
 
-//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//        {
-//            if (!optionsBuilder.IsConfigured)
-//            {
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-//                optionsBuilder.UseNpgsql("User ID=test;Password=test;Host=127.0.0.1;Port=5050;Database=test;");
-//            }
-//        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "en_US.utf8");
